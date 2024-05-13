28bbfcf9580bd0355c192a2bcab023d1db70c7fa
StarbugStone <pint2stone@gmail.com>
2024-05-13 16:19:59 +0200

    fix: removing daysn and hours for the countdown

--- code/public/js/countdown.js
+++ code/public/js/countdown.js
@@ -37,14 +37,14 @@ function handleCountdownElement() {
         var distance = countdownDate - now;
 
         // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         // Display the result in an element with id="countdown"
-        countdownElement.innerHTML = days + "d " + hours + "h "
-            + minutes + "m " + seconds + "s ";
+        // countdownElement.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+        countdownElement.innerHTML = minutes + "m " + seconds + "s ";
 
         // If the count down is over, write some text
         if (distance < 0) {
