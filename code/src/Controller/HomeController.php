066aff5c22377cccaf4623cdc5449de5daf6b282
StarbugStone <pint2stone@gmail.com>
2024-05-15 19:21:02 +0200

    fix: making the player list a json call called every minute

--- code/src/Controller/HomeController.php
+++ code/src/Controller/HomeController.php
@@ -4,27 +4,36 @@ namespace App\Controller;
 
 use App\Service\CookieJarService;
 use App\Service\PzRconService;
-use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
+    private PzRconService $pzRcon;
+    private CookieJarService $cookieJarService;
+
+    public function __construct(PzRconService $pzRcon, CookieJarService $cookieJarService)
+    {
+        $this->pzRcon = $pzRcon;
+        $this->cookieJarService = $cookieJarService;
+    }
+
     #[Route('/', name: 'app_home')]
-    public function index(PzRconService $pzRcon, CookieJarService $cookieJarService, Request $request): Response
+    public function index(Request $request): Response
     {
         $steamUser = $request->cookies->get('steam_user', '');
 
-        $playerStats = $pzRcon->getPlayerInfo();
+        $playerStats = $this->pzRcon->getPlayerInfo();
         $playerCount = $playerStats['playerCount'];
         $players = $playerStats['players'];
 
-        $cookieJars = $cookieJarService->getCookieJarHistory();
-        $isCookieJarAvailable = $cookieJarService->isCookieJarAvailable();
+        $cookieJars = $this->cookieJarService->getCookieJarHistory();
+        $isCookieJarAvailable = $this->cookieJarService->isCookieJarAvailable();
 
-        $isoDate = $cookieJarService->nextCookieAvailable()->format('c'); // Converts to ISO 8601 format
+        $isoDate = $this->cookieJarService->nextCookieAvailable()->format('c'); // Converts to ISO 8601 format
         $nextCookieJarAvailable = $isoDate;
 
         return $this->render('home/index.html.twig', [
@@ -35,6 +44,21 @@ class HomeController extends AbstractController
             'isCookieJarAvailable' => $isCookieJarAvailable,
             'nextCookieJarAvailable' => $nextCookieJarAvailable,
             'steam_user' => $steamUser,
+        ]);
+    }
+
+
+    #[Route('/api/players', name: 'api_players')]
+    public function getPlayers()
+    {
+        $playerStats = $this->pzRcon->getPlayerInfo();
+        // Get player data from your database or service
+        $playerCount = $playerStats['playerCount'];
+        $players = $playerStats['players'];
+
+        return new JsonResponse([
+            'playerCount' => $playerCount,
+            'players' => $players
         ]);
     }
 }
